c5e93f51072aba38e0c1df5175e723a03f35a79c
John Puddephatt <john@jdp.org.uk>
2022-11-26 19:11:54 +0000

    fix home service layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -74,69 +74,68 @@
               class="text-green-dark border-green-dark mt-8 inline-block rounded-xl border-2 py-3 px-6 font-bold lowercase">read
               more</span>
           </div>
+        </a>
+      @endforeach
     </div>
-    </a>
-    @endforeach
-  </div>
 
-  </div>
+    <div class="relative py-36">
+      <div class="container relative">
+        <span class="bg-warm-white absolute right-36 -top-12 z-10 block h-36 w-36 rounded-full"></span>
+        <span class="bg-strategy-light absolute right-72 -top-24 z-10 block h-20 w-20 rounded-full"></span>
 
-  <div class="relative py-36">
-    <div class="container relative">
-      <span class="bg-warm-white absolute right-36 -top-12 z-10 block h-36 w-36 rounded-full"></span>
-      <span class="bg-strategy-light absolute right-72 -top-24 z-10 block h-20 w-20 rounded-full"></span>
+        <div class="bg-pink-white relative grid grid-cols-2 overflow-hidden rounded-3xl">
 
-      <div class="bg-pink-white relative grid grid-cols-2 overflow-hidden rounded-3xl">
-
-        <div class="p-16 pr-8">
-          <figure class="text-green-dark">
-            <blockquote class="font-serif text-3xl">
-              “Lemon Balm brought an impressive and detailed understanding of Yorkshire Water’s vision to take
-              responsibility for the water environment”
-            </blockquote>
-            <figcaption class="mt-4 font-bold">
-              <p class="text-xl">Richard Flint,</p>
-              CEO Yorkshire Water
-            </figcaption>
-          </figure>
-          <a class="text-green-dark border-green-dark mt-8 inline-block rounded-xl border-2 py-3 px-6 font-bold lowercase"
-            href="#">read more</a>
+          <div class="p-16 pr-8">
+            <figure class="text-green-dark">
+              <blockquote class="font-serif text-3xl">
+                “Lemon Balm brought an impressive and detailed understanding of Yorkshire Water’s vision to take
+                responsibility for the water environment”
+              </blockquote>
+              <figcaption class="mt-4 font-bold">
+                <p class="text-xl">Richard Flint,</p>
+                CEO Yorkshire Water
+              </figcaption>
+            </figure>
+            <a class="text-green-dark border-green-dark mt-8 inline-block rounded-xl border-2 py-3 px-6 font-bold lowercase"
+              href="#">read more</a>
+          </div>
+          <div><img class="bg-red-light block h-full w-full object-cover object-center"></div>
         </div>
-        <div><img class="bg-red-light block h-full w-full object-cover object-center"></div>
       </div>
+      <svg xmlns="http://www.w3.org/2000/svg" class="absolute left-0 -bottom-72 -z-10" width="417.47" height="629.36"
+        viewBox="0 0 417.47 629.36">
+        <path fill="#c3deda" opacity="0.2"
+          d="M-1.77,4878c41.47,22.4,89.6,35.26,140.28,36.51,199.2,4.91,285.3-159.34,276.6-303.11s-151.06-93.28-316.33-301C69.14,4273.16,34.09,4280.26.17,4312"
+          transform="translate(1.77 -4285.23)" />
+      </svg>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" class="absolute left-0 -bottom-72 -z-10" width="417.47" height="629.36"
-      viewBox="0 0 417.47 629.36">
-      <path fill="#c3deda" opacity="0.2"
-        d="M-1.77,4878c41.47,22.4,89.6,35.26,140.28,36.51,199.2,4.91,285.3-159.34,276.6-303.11s-151.06-93.28-316.33-301C69.14,4273.16,34.09,4280.26.17,4312"
-        transform="translate(1.77 -4285.23)" />
+
+    <svg class="text-warm-white h-auto min-w-full" xmlns="http://www.w3.org/2000/svg" width="1258" height="123.77"
+      viewBox="0 0 1258 123.77">
+      <path fill="currentColor"
+        d="M1100.75,75.29C1022.12,75.29,1022.12,0,943.5,0S864.87,75.29,786.25,75.29,707.62,0,629,0,550.37,75.29,471.75,75.29,393.12,0,314.5,0,235.87,75.29,157.25,75.29,78.62,0,0,0V124.13l1258-.36V0C1179.37,0,1179.37,75.29,1100.75,75.29Z" />
     </svg>
-  </div>
 
-  <svg class="text-warm-white h-auto min-w-full" xmlns="http://www.w3.org/2000/svg" width="1258" height="123.77"
-    viewBox="0 0 1258 123.77">
-    <path fill="currentColor"
-      d="M1100.75,75.29C1022.12,75.29,1022.12,0,943.5,0S864.87,75.29,786.25,75.29,707.62,0,629,0,550.37,75.29,471.75,75.29,393.12,0,314.5,0,235.87,75.29,157.25,75.29,78.62,0,0,0V124.13l1258-.36V0C1179.37,0,1179.37,75.29,1100.75,75.29Z" />
-  </svg>
-
-  <div class="bg-warm-white">
-    <div class="container">
-      <h2 class="text-red-dark mb-8 font-serif text-5xl">Have a project in mind?</h2>
-      <p class="mb-2 max-w-md text-lg">We are based in Meanwood in Leeds but work with people and organisations all across
-        the
-        country.</p>
-      <p class="mb-2 max-w-md text-lg">If you are interested in what we do, have a project in mind or would like to have a
-        chat, send us a message.</p>
-    </div>
-
-    <div class="container grid grid-cols-2">
-      <div>
-        {!! do_shortcode('[contact-form-7 id="5" title="Contact form"]') !!}
+    <div class="bg-warm-white">
+      <div class="container">
+        <h2 class="text-red-dark mb-8 font-serif text-5xl">Have a project in mind?</h2>
+        <p class="mb-2 max-w-md text-lg">We are based in Meanwood in Leeds but work with people and organisations all
+          across
+          the
+          country.</p>
+        <p class="mb-2 max-w-md text-lg">If you are interested in what we do, have a project in mind or would like to have
+          a
+          chat, send us a message.</p>
       </div>
-      <div>
-        <img class="-ml-32 -mt-12 block w-[calc(100%+12rem)] max-w-none" src="@asset('/images/yoga.png')" />
-      </div>
-    </div>
 
-  </div>
-@endsection
+      <div class="container grid grid-cols-2">
+        <div>
+          {!! do_shortcode('[contact-form-7 id="5" title="Contact form"]') !!}
+        </div>
+        <div>
+          <img class="-ml-32 -mt-12 block w-[calc(100%+12rem)] max-w-none" src="@asset('/images/yoga.png')" />
+        </div>
+      </div>
+
+    </div>
+  @endsection
